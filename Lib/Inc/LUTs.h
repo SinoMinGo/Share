08be7bd0f9e0f29ce8a61d81f9ee6f77dcc0c366
SinoMinGo <835814714@qq.com>
2026-03-12 21:42:51 +0800

    Add files via upload

---
+++ Lib/Inc/LUTs.h
@@ -0,0 +1,10 @@
+#ifndef LUTS_H
+#define LUTS_H
+
+#include <stdint.h>
+
+extern const uint32_t Wave_LUT_SIN[128];
+extern const uint32_t Wave_LUT_TRG[128];
+extern const uint32_t Sine12bit_128pts[128];
+
+#endif
